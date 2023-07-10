01943917c879386abc1082280d8e0bd63cac0410
yiğit budak <yibudak@gmail.com>
2023-07-10 16:16:35 +0300

    [ADD] connector.py, readme, demo json file

---
+++ README.md
@@ -0,0 +1,63 @@
+# Dahua-Alarm-Center-Selenium
+
+## Overview
+"Dahua-Alarm-Center-Selenium" is a Python script developed to automate the process of monitoring and logging alarms from the Dahua Alarm Center using Selenium WebDriver. The script is designed to log in to the Dahua Alarm Center, navigate to the alarm center, enable alarm tracking, fetch alarms, and print new alarms' data.
+
+## Features
+- Automatic login to Dahua Alarm Center
+- Navigation to the Alarm Center page
+- Enabling alarm tracking
+- Fetching and displaying new alarms
+- After fetching 50 alarms, it restarts the whole process to prevent memory overload.
+
+## Installation
+Clone the repository:
+
+```
+git clone https://github.com/yibudak/Dahua-Alarm-Center-Selenium.git
+```
+
+Change your directory:
+
+```
+cd Dahua-Alarm-Center-Selenium
+```
+
+Install the requirements:
+
+```
+python3 -m pip install -r requirements.txt
+```
+
+## Usage
+
+You can run the script with required arguments: host, username, password, and camera-list.
+
+Example:
+
+```
+python3 connector.py --host yourHost --username yourUsername --password yourPassword --camera-list cameraList.json
+```
+
+## Required Arguments
+
+- **--host**: The host URL of the Dahua Alarm Center. (eg: 192.168.1.100)
+- **--username**: The username used for login.
+- **--password**: The password used for login.
+- **--camera-list**: The list of cameras to monitor in JSON format.
+
+## Dependencies
+
+Firefox and geckodriver are required to run the script.
+
+Make sure to install these dependencies before running the script.
+
+## Notes
+
+This project is a work in progress and subject to change. Please make sure to pull the latest changes from the master branch before use.
+
+## Contributing
+We appreciate all contributions. If you're planning to contribute back bug fixes or features, please file an issue describing what you're fixing or feature you're adding.
+
+## License
+This project is licensed under AGPL-3.0 License - see the [LICENSE](LICENSE) file for details.
---
+++ connector.py
@@ -0,0 +1,164 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+import argparse
+import json
+import time
+
+"""
+Yiğit Budak (https://github.com/yibudak/Dahua-Alarm-Center-Selenium)
+Dahua Alarm Center Telegram Connector
+"""
+
+
+options = Options()
+# Dahua Alarm Center only works with Internet Explorer
+options.set_preference(
+    "general.useragent.override",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko",
+)
+
+
+def load_camera_list(camera_list):
+    """Load Camera List"""
+    try:
+        loaded_camera_list = json.loads(camera_list)
+    except Exception as e:
+        print("Loading Camera List Failed. Error:\n %s" % str(e))
+        exit()
+    return loaded_camera_list
+
+
+def login(host, browser, username, password):
+    """Login to Dahua Alarm Center"""
+    browser.get(f"http://{host}")
+    try:
+        # Username
+        username_field = browser.find_element(By.XPATH, "//input[@id='username']")
+        username_field.clear()
+        username_field.send_keys(username)
+        # Password
+        password_field = browser.find_element(By.XPATH, "//input[@id='password']")
+        password_field.clear()
+        password_field.send_keys(password)
+        # Login
+        login_button = browser.find_element(By.XPATH, "//a[@id='ulgin']")
+        login_button.click()
+        time.sleep(3)
+    except Exception as e:
+        print("Login Failed. Error:\n %s" % str(e))
+        browser.close()
+        exit()
+    return True
+
+
+def navigate_to_alarm_center(browser):
+    """Navigate to Alarm Center"""
+    try:
+        # Alarm Center
+        alarm_center_button = browser.find_element(By.XPATH, "//a[@id='xbjsz']")
+        alarm_center_button.click()
+        time.sleep(3)
+    except Exception as e:
+        print("Navigation to Alarm Center Failed. Error:\n %s" % str(e))
+        browser.close()
+        exit()
+    return True
+
+
+def enable_alarm_tracking(browser):
+    """Enable Alarm Tracking"""
+    try:
+        # Alarm Tracking
+        alarm_tracking_button = browser.find_element(
+            By.XPATH, "//input[@id='alarmType_1']"
+        )
+        alarm_tracking_button.click()
+        time.sleep(1)
+    except Exception as e:
+        print("Enable Alarm Tracking Failed. Error:\n %s" % str(e))
+        browser.close()
+        exit()
+    return True
+
+
+def fetch_alarms(camera_list, browser):
+    """Fetch Alarms"""
+    found_signals = {}
+    alarm_frame = browser.find_element(By.XPATH, "//iframe[@id='alarm_frame']")
+    browser.switch_to.frame(alarm_frame)
+    while True:
+        signals = browser.find_elements(By.XPATH, "//div")
+        for signal in signals:
+            splitted_text = signal.text.split("\n")
+            if len(splitted_text) == 4 and splitted_text[0] not in found_signals:
+                camera_name = camera_list.get(splitted_text[3], False)
+                data = {
+                    "date": splitted_text[1],
+                    "alarm_type": splitted_text[2],
+                    "camera_name": camera_name or splitted_text[3],
+                }
+                found_signals[splitted_text[0]] = data
+                print(f"New Alarm: {data}")
+            else:
+                pass
+            if len(found_signals) >= 50:
+                # If there are more than 50 alarms, restart the whole process
+                return True
+
+        time.sleep(3)  # Slow down the loop to avoid overloading the server
+
+
+def main(host, username, password, camera_list):
+    # Load the items
+    try:
+        json_file = open(camera_list, "r")
+        loaded_camera_list = json.load(json_file)
+    except FileNotFoundError:
+        print("Camera List File Not Found")
+        exit()
+
+    # Open the browser
+    driver = webdriver.Firefox(options=options)
+
+    # Methods
+    while True:
+        # After reaching 50 signals, we start a new session,
+        # thus avoiding memory bloat.
+        login(host, driver, username, password)
+        navigate_to_alarm_center(driver)
+        enable_alarm_tracking(driver)
+        fetch_alarms(camera_list=loaded_camera_list, browser=driver)
+        time.sleep(3)  # Slow down the loop to avoid overloading the server
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Dahua Alarm Center Telegram Connector"
+    )
+    parser.add_argument(
+        "--host",
+        type=str,
+        help="Host URL",
+        required=True,
+    )
+    parser.add_argument(
+        "--username",
+        type=str,
+        help="Username for login",
+        required=True,
+    )
+    parser.add_argument(
+        "--password",
+        type=str,
+        help="Password for login",
+        required=True,
+    )
+    parser.add_argument(
+        "--camera-list",
+        type=str,
+        help="List of cameras to monitor (JSON)",
+        required=True,
+    )
+    args = parser.parse_args()
+    main(args.host, args.username, args.password, args.camera_list)
